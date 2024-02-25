5c01a911974ebc5fea7a3db777942d89cb4cd0ec
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-02-25 10:01:01 -0300

    feat: add ngmodel for heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,17 @@
-import { Component, WritableSignal, signal } from '@angular/core';
-import { Hero } from '../hero';
 import { UpperCasePipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'toh-heroes',
   standalone: true,
-  imports: [UpperCasePipe],
+  imports: [UpperCasePipe, FormsModule],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss',
 })
 export class HeroesComponent {
-  hero: WritableSignal<Hero> = signal({
+  hero = signal<Hero>({
     id: 1,
     name: 'Windstorm',
   });
